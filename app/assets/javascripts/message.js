75fae2384b75a2b245d95c2757a9f51a55c240b6
takasutakaji <63281696+takasutakaji@users.noreply.github.com>
2020-05-23 21:14:01 +0900

    非同期通信実装完了

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,62 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message-box">
+        <div class="message-box__user-name">
+          ${message.user_name}
+        </div>
+        <div class="messages__date">
+          ${message.created_at}
+        </div>
+      </div>
+      <div class="message-text">
+        <p class="lower-message__content">
+          ${message.content}
+        </p>
+      </div>
+      <img src=${message.image} >`
+     return html;
+   } else {
+     var html = 
+      `<div class="message-box">
+           <div class="message-box__user-name">
+             ${message.user_name}
+           </div>
+           <div class="messages__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="message-text">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+       </div>`
+     return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('form')[0].reset();
+    $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+    $(button).prop('disabled', false);
+  })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+  });
+  return false;
+})
+});
